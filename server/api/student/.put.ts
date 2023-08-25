04cc4ac4ace09a259377fd61031040721d60569d
russell <russelldossouyovo@gmail.com>
2023-08-25 14:01:38 +0100

    no prob

--- server/api/student/.put.ts
+++ server/api/student/.put.ts
@@ -11,6 +11,7 @@ export default defineEventHandler(async (event) => {
       input_data[key] = body[key];
     }
   }
+  // console.log("input_data: ----------------------\n", input_data);
 
   // check is the profil is completed
   const student = await prisma.student.findUnique({
