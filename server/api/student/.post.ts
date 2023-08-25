1773e2fb0b43570bcad5dfaac7fb5fd718be63cd
russell <russelldossouyovo@gmail.com>
2023-08-25 17:18:45 +0100

    .

--- server/api/student/.post.ts
+++ server/api/student/.post.ts
@@ -11,6 +11,7 @@ export default defineEventHandler(async (event) => {
       input_data[key] = body[key];
     }
   }
+  // console.log("input_data: ----------------------\n", input_data);
 
   // case where we want to create a student
   if (
