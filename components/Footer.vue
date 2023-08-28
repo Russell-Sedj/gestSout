38d13cc284c76410993b360f290436638dc92a52
russell <russelldossouyovo@gmail.com>
2023-08-28 12:53:31 +0100

    send footer to a component

---
+++ components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <footer
+      class="flex justify-between items-center text-xs px-2 h-44 relative bottom-0 w-full z-10 bg-gray-800 text-white"
+    >
+      <p>&copy;2021 Easy Manager. All rights reserved.</p>
+      <div>
+        <p class="text-center">+229 12 34 56 78</p>
+        <p class="text-center">easymanager@gmail.com</p>
+      </div>
+    </footer>
+  </div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -6,14 +6,6 @@
       <slot />
     </main>
 
-    <footer
-      class="flex justify-between items-center text-xs px-2 h-44 relative bottom-0 w-full z-10 bg-gray-800 text-white"
-    >
-      <p>&copy;2021 Easy Manager. All rights reserved.</p>
-      <div>
-        <p class="text-center">+229 12 34 56 78</p>
-        <p class="text-center">easymanager@gmail.com</p>
-      </div>
-    </footer>
+    <Footer />
   </div>
 </template>
