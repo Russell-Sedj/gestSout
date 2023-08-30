423bc10435faaf146db2064bd30a8c713e006d75
russell <russelldossouyovo@gmail.com>
2023-08-30 12:04:37 +0100

    add password modif

--- pages/student/profilStudent.vue
+++ pages/student/profilStudent.vue
@@ -58,6 +58,23 @@
             >
           </div>
 
+          <div class="relative z-0 w-full mb-8 group">
+            <input
+              type="password"
+              name="floating_password"
+              id="floating_password"
+              class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              required
+              v-model="student.password"
+            />
+            <label
+              for="floating_password"
+              class="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-8 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-8"
+              >Mot de passe</label
+            >
+          </div>
+
           <div class="grid md:grid-cols-2 md:gap-6">
             <div class="relative z-0 w-full mb-8 group">
               <input
@@ -189,6 +206,7 @@ const student = ref({
   lastname: currentUser.value.lastname,
   firstname: currentUser.value.firstname,
   email: currentUser.value.email,
+  password: currentUser.value.password,
   telephone: currentUser.value.telephone,
   address: currentUser.value.address,
   field: currentUser.value.field,
@@ -205,6 +223,7 @@ async function editStudent(student) {
     student.firstname &&
     student.lastname &&
     student.email &&
+    student.password &&
     student.telephone &&
     student.address &&
     student.field
