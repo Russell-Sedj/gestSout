95e29c94a15df9f10d2c4e5197d7c9683835bbdb
russell <russelldossouyovo@gmail.com>
2023-08-30 13:56:04 +0100

    add programmation link to header

--- components/ToggleMenu.vue
+++ components/ToggleMenu.vue
@@ -86,6 +86,10 @@
         >Acceuil</nuxt-link
       >
 
+      <nuxt-link class="block px-4 py-2 hover:bg-gray-100" to="/programmation"
+        >Programmation</nuxt-link
+      >
+
       <nuxt-link
         class="block px-4 py-2 hover:bg-gray-100"
         v-if="loggedIn"
