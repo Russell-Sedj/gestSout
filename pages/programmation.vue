95e1790608e827b35ad2dc514a9a87707f52de2d
russell <russelldossouyovo@gmail.com>
2023-08-30 16:56:04 +0100

    programmation init

--- pages/programmation.vue
+++ pages/programmation.vue
@@ -1,5 +1,36 @@
 <template>
-  <div>hey</div>
+  <div>
+    <div v-for="student in studentList">
+      <div>{{ student }}</div>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+const runtimeConfig = useRuntimeConfig();
+const currentYear = runtimeConfig.public.currentYear;
+
+const currentUser = ref(null);
+currentUser.value = await $fetch("/api/me");
+
+// get the student's university
+const myUniversity = ref(null);
+const studentList = ref(null);
+onMounted(async () => {
+  if (currentUser.value.role === "student") {
+    await $fetch("/api/direction/", {
+      method: "POST",
+      body: { uniqueId: currentUser.value.directionId },
+    }).then(async (res) => {
+      myUniversity.value = res;
+
+      // get the list of students from the university
+      const { data } = await useFetch("/api/student/", {
+        method: "POST",
+        body: { listDirectionId: myUniversity.value.id, year: currentYear },
+      });
+      studentList.value = data.value.request;
+    });
+  }
+});
+</script>
