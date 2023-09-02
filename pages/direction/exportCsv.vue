1cc06c5b84f5d55e9a349494bd133968368d1378
russell <russelldossouyovo@gmail.com>
2023-09-02 19:19:28 +0100

    add formating and it's working

--- pages/direction/exportCsv.vue
+++ pages/direction/exportCsv.vue
@@ -1,11 +1,26 @@
 <template>
   <div>
-    <button @click="exportDataToCsv">Click</button>
+    <p>
+      Si le telechargement ne se lance pas automatiquement, cliquez
+      <span
+        class="text-blue-500 cursor-pointer"
+        @click="exportDataToCsv(studentList)"
+        >ici</span
+      >
+    </p>
   </div>
 </template>
 
 <script setup>
-const converToCsv = (data) => {
+// get student list
+const currentUser = ref(null);
+currentUser.value = await $fetch("/api/me");
+
+const runtimeConfig = useRuntimeConfig();
+const currentYear = runtimeConfig.public.currentYear;
+
+// convert data to csv
+const convertToCsv = (data) => {
   const headers = Object.keys(data[0]);
   const rows = data.map((obj) => headers.map((header) => obj[header]));
   const headerRow = headers.join(",");
@@ -13,28 +28,48 @@ const converToCsv = (data) => {
   return csvRows.join("\n");
 };
 
-const exportDataToCsv = () => {
-  const data = [
-    {
-      name: "John",
-      age: 24,
-      city: "New York",
-      country: "USA",
-    },
-    {
-      name: "Mary",
-      age: 32,
-      city: "Paris",
-      country: "France",
-    },
-  ];
-
-  const csvContent = converToCsv(data);
+const exportDataToCsv = (data) => {
+  const csvContent = convertToCsv(data);
   const blob = new Blob([csvContent], { type: "text/csv;charset=utf-8;" });
   const url = URL.createObjectURL(blob);
   const link = document.createElement("a");
   link.href = url;
   link.setAttribute("download", "export_data.csv");
   link.click();
+  //   window.location.href = "/direction/";
 };
+
+// convert in a good csv format
+function escapeCsvText(text) {
+  // Escape double quotes by doubling them up
+  const escapedText = text.replace(/"/g, '""');
+  // Wrap the text in double quotes if it contains special characters or commas
+  if (escapedText.includes(",") || escapedText.includes("\n")) {
+    return `"${escapedText}"`;
+  }
+  return escapedText;
+}
+
+// format some property of the object i got from the api
+const formatStudentList = () => {
+  studentList.forEach((student) => {
+    student.appreciation = student.appreciation
+      ? escapeCsvText(student.appreciation)
+      : "";
+  });
+};
+
+// get list of student
+let studentList = [];
+await $fetch("/api/student/", {
+  method: "POST",
+  body: { listDirectionId: currentUser.value.id, year: currentYear },
+}).then((data) => {
+  studentList = data.request;
+});
+
+onMounted(() => {
+  formatStudentList();
+  // exportDataToCsv(studentList);
+});
 </script>
